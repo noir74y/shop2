98f7aff6aad7fa900b4b1d62810009d604518c24
noir74y <noir74@yandex.ru>
2025-04-25 22:17:00 +0300

    to remove webmvc

--- src/main/java/ru/noir74/shop/ShopApplication.java
+++ src/main/java/ru/noir74/shop/ShopApplication.java
@@ -2,8 +2,13 @@ package ru.noir74.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "ru\\.noir74\\.shop\\.controllers\\..*")
+})
 public class ShopApplication {
 
     public static void main(String[] args) {
