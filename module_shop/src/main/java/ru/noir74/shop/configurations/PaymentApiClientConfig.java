326753c70a3da8dbaad21701fb2d9b01469a6bde
noir74y <noir74@yandex.ru>
2025-06-11 23:25:25 +0300

    feat: to add api client to module_shop

---
+++ module_shop/src/main/java/ru/noir74/shop/configurations/PaymentApiClientConfig.java
@@ -0,0 +1,25 @@
+package ru.noir74.shop.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.noir74.shop.client.api.PaymentApi;
+import ru.noir74.shop.client.clients.ApiClient;
+
+@Configuration
+public class PaymentApiClientConfig {
+    @Value("${payment-service.url}")
+    private String paymentServiceBaseUrl;
+
+    @Bean
+    public ApiClient paymentApiClient() {
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(paymentServiceBaseUrl);
+        return apiClient;
+    }
+
+    @Bean
+    public PaymentApi paymentApi(ApiClient paymentApiClient) {
+        return new PaymentApi(paymentApiClient);
+    }
+}
