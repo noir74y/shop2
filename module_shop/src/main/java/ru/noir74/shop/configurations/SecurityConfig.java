74cbfe8b9a8c5506cb144a9936122f318edf5011
noir74y <noir74@yandex.ru>
2025-06-25 00:13:10 +0300

    feat: spring security has been added to project

---
+++ module_shop/src/main/java/ru/noir74/shop/configurations/SecurityConfig.java
@@ -0,0 +1,81 @@
+package ru.noir74.shop.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcReactiveOAuth2UserService;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.ReactiveOAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.jwk-set-uri}")
+    private String JWK_SET_URI;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        //.pathMatchers("/hello").hasRole("MANAGER")
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(oauth2Login -> {
+                })
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+
+    @Bean
+    public ReactiveOAuth2UserService<OidcUserRequest, OidcUser> oidcReactiveUserService() {
+        final OidcReactiveOAuth2UserService oidcUserService = new OidcReactiveOAuth2UserService();
+
+        return (oidcUserRequest) -> oidcUserService.loadUser(oidcUserRequest)
+                .flatMap(oidcUser -> {
+                    Set<GrantedAuthority> mappedAuthorities = new HashSet<>(oidcUser.getAuthorities());
+                    OAuth2AccessToken accessToken = oidcUserRequest.getAccessToken();
+
+                    return jwtDecoder().decode(accessToken.getTokenValue())
+                            .doOnError(e -> System.err.println("Ошибка при декодировании Access Token: " + e.getMessage()))
+                            .map(decodedAccessToken -> {
+                                Map<String, Object> accessTokenClaims = decodedAccessToken.getClaims();
+
+                                if (accessTokenClaims.get("resource_access") instanceof Map<?, ?> resourceAccessInAccessToken) {
+                                    if (resourceAccessInAccessToken.get("shop-application") instanceof Map<?, ?> clientAccessInAccessToken) {
+                                        if (clientAccessInAccessToken.get("roles") instanceof Collection<?> clientRoles) {
+                                            clientRoles.forEach(role -> {
+                                                if (role instanceof String) {
+                                                    mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + ((String) role).toUpperCase()));
+                                                }
+                                            });
+                                        }
+                                    }
+                                }
+                                return new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+                            })
+                            .onErrorResume(e -> Mono.just(new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo())));
+                });
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(JWK_SET_URI).build();
+    }
+}
